7b8b709f2ec1fe9a7a6857432ec5980bc8600afb
camiloee <camilo.elizondo@gmail.com>
2025-07-31 20:47:50 -0400

    version borrador

---
+++ README.md
@@ -0,0 +1,7 @@
+<h1>Readme Evaluador</h1> 
+
+Mi page : https://aka-milo.github.io/
+
+Fork 1: https://github.com/AKA-milo/Prueba1M1
+
+Fork 2: https://github.com/AKA-milo/BudcatCode.github.io
---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const typed = new Typed(".typed-text", {
+    strings: ["Estudio de Grabación", "Producción Musical", "Masterización", "Beats Instrumentales"],
+    typeSpeed: 60,
+    backSpeed: 40,
+    backDelay: 1000,
+    loop: true,
+    smartBackspace: true,
+  });
+});
